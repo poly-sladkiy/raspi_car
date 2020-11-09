755c6b457cdd3960ccf201a857fc1a1a757042d3
Цветков Николай <nmcvetkov@yandex.ru>
2020-11-09 16:44:38 +0300

    Сделал функцию, выводящую информацию о расположении объекта

--- Develop/ball_following_yellow.py
+++ Develop/ball_following_yellow.py
@@ -18,58 +18,64 @@ maximum_area = 100000
 HUE_VAL = 29
 
 lower_color = np.array([HUE_VAL - 10, 100, 100])
-upper_color = np.array([HUE_VAL+10, 255, 255])
- 
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    image = frame.array
+upper_color = np.array([HUE_VAL + 10, 255, 255])
 
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
- 
-    color_mask = cv2.inRange(hsv, lower_color, upper_color)
- 
-    countours, _ = cv2.findContours(color_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
- 
-    object_area = 0
-    object_x = 0
-    object_y = 0
- 
-    for contour in countours:
 
-        x, y, width, height = cv2.boundingRect(contour)
-        found_area = width * height
-        center_x = x + (width / 2)
-        center_y = y + (height / 2)
+def LocationOfObject():
 
-        if object_area < found_area:
-            object_area = found_area
-            object_x = center_x
-            object_y = center_y
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        image = frame.array
 
-    if object_area > 0:
-        yellow_object_location = [object_area, object_x, object_y]
-        
-    else:
-        yellow_object_location = None
- 
-    if yellow_object_location:
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-        if (yellow_object_location[0] > minimum_area) and (yellow_object_location[0] < maximum_area):
+        color_mask = cv2.inRange(hsv, lower_color, upper_color)
 
-            if yellow_object_location[1] > (center_image_x + (image_width/3)):
-                print("Turning right")
+        countours, _ = cv2.findContours(color_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-            elif yellow_object_location[1] < (center_image_x - (image_width/3)):
-                print("Turning left")
+        object_area = 0
+        object_x = 0
+        object_y = 0
 
-            else:
-                print("Forward")
+        for contour in countours:
 
-        elif yellow_object_location[0] < minimum_area:
-            print("Target isn't large enough, searching")
+            x, y, width, height = cv2.boundingRect(contour)
+            found_area = width * height
+            center_x = x + (width / 2)
+            center_y = y + (height / 2)
+
+            if object_area < found_area:
+                object_area = found_area
+                object_x = center_x
+                object_y = center_y
+
+        if object_area > 0:
+            yellow_object_location = [object_area, object_x, object_y]
 
         else:
-            print("Target large enough, stopping")
-    else:
-        print("Target not found, searching")
+            yellow_object_location = None
+
+        if yellow_object_location:
+
+            if (yellow_object_location[0] > minimum_area) and (yellow_object_location[0] < maximum_area):
+
+                if yellow_object_location[1] > (center_image_x + (image_width/3)):
+                    print("Turning right")
+                    #return "Turning_right"
+                elif yellow_object_location[1] < (center_image_x - (image_width/3)):
+                    print("Turning left")
+                    #return "Turning_left"
+                else:
+                    print("Forward")
+                    #return "Forward"
+
+            elif yellow_object_location[0] < minimum_area:
+                print("Target isn't large enough, searching")
+                #return "Target isn't large enough, searching"
+            else:
+                print("Target large enough, stopping")
+                #return "Target large enough, stopping"
+        else:
+            print("Target not found, searching")
+            #return "Target not found, searching"
 
     rawCapture.truncate(0)
