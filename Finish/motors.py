deb2f8b136ea5aba98b9699b96d9005f7ad9f255
polySladkiy <ignakov37@gmail.com>
2020-12-14 09:52:25 +0300

    PiCar follows to the person with 12-16 fps

--- Finish/motors.py
+++ Finish/motors.py
@@ -3,7 +3,7 @@ import RPi.GPIO as gpio
 
 class PiCar:
 
-    def __init__(self):
+    def __init__(self, duty_cycle=100, hz=100):
         gpio.setmode(gpio.BCM)
 
         gpio.setup(17, gpio.OUT)  # ENA
@@ -14,27 +14,27 @@ class PiCar:
         gpio.setup(16, gpio.OUT)  # IN 3
         gpio.setup(20, gpio.OUT)  # IN 4
 
-        self.duty_cycle = 70
-        self.hz = 50
+        self.duty_cycle = duty_cycle
+        self.hz = hz
 
         self.pwm_a = gpio.PWM(17, self.hz)
         self.pwm_b = gpio.PWM(21, self.hz)
 
     @staticmethod
     def forward() -> None:
-        gpio.output(27, 0)  # IN 1
-        gpio.output(22, 1)  # IN 2
-
-        gpio.output(16, 1)  # IN 3
-        gpio.output(20, 0)  # IN 4
-
-    @staticmethod
-    def reverse() -> None:
         gpio.output(27, 1)  # IN 1
         gpio.output(22, 0)  # IN 2
 
         gpio.output(16, 0)  # IN 3
         gpio.output(20, 1)  # IN 4
+
+    @staticmethod
+    def reverse() -> None:
+        gpio.output(27, 0)  # IN 1
+        gpio.output(22, 1)  # IN 2
+
+        gpio.output(16, 1)  # IN 3
+        gpio.output(20, 0)  # IN 4
 
     @staticmethod
     def right() -> None:
